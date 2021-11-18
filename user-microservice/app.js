5dfa3069d1bf25473d370a59542a4c984c27e314
Manthan Mehta <manthanmehta810@gmail.com>
2021-11-19 03:26:52 +0530

    Flight booking project

---
+++ user-microservice/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const passport = require("passport");
+// const cookieSession = require("cookie-session");
+// require("./passport");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+if (!process.env.NODE_ENV === "test") {
+  app.use(morgan("dev"));
+}
+app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(
+//   cookieSession({
+//     name: "session",
+//     keys: ["key1", "key2"],
+//   })
+// );
+
+const uri = process.env.ATLAS_URI;
+if (!process.env.NODE_ENV === "test") {
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+} else {
+  const testUri = process.env.ATLAS_URI_TEST;
+  mongoose.connect(testUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+}
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const userRouter = require("./routes/users");
+
+app.use("/", userRouter);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+module.exports = app;
